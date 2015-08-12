de3278d531b9c1485b42e63c899cd4d9b9c860bd
Zack Hsi <zhsi@lyft.com>
2015-08-12 11:00:50 -0700

    Manage flake8 file

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@ IGNORED_FILES=(tags $0)
 
 for file in *
 do
+    link_from=$(pwd)/$file
     if [[ "${IGNORED_FILES[@]}" =~ "${file} " || "${IGNORED_FILES[${#IGNORED_FILES[@]}-1]}" == "${file}" ]]; then
-        # whatever you want to do when arr contains value
         echo "Ignoring $file..."
     else
-        link_from=$(pwd)/$file
         link_to=~/.$file
+        if [[ "$file" == "flake8" ]]; then
+            link_to=~/.config/flake8
+        fi
+
         if [ ! -L $link_to ]; then
             echo "Linking $link_from to $link_to..."
             ln -s $link_from $link_to
