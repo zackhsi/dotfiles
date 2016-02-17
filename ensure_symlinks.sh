5fd9a8dead20f74af77027449171817405177e21
Zack Hsi <zackhsi@gmail.com>
2016-02-16 21:06:53 -0800

    Shellcheck

--- ensure_symlinks.sh
+++ ensure_symlinks.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 IGNORED_FILES=(
   $0
   Brewfile
@@ -10,18 +12,18 @@ IGNORED_FILES=(
 
 for file in *
 do
-    if ! [[ "${IGNORED_FILES[@]}" =~ "${file} " || "${IGNORED_FILES[${#IGNORED_FILES[@]}-1]}" == "${file}" ]]; then
-        link_from=$(pwd)/$file
-        link_to=~/.$file
-        if [[ "$file" == "flake8" ]]; then
-            link_to=~/.config/flake8
-        elif [[ "$file" == "sshconfig" ]]; then
-            link_to=~/.ssh/config
-        fi
-
-        if [ ! -L $link_to ]; then
-            echo "Linking $link_from to $link_to..."
-            ln -s $link_from $link_to
-        fi
+  if ! [[ "${IGNORED_FILES[@]}" =~ ${file} || "${IGNORED_FILES[${#IGNORED_FILES[@]}-1]}" == "${file}" ]]; then
+    link_from=$(pwd)/$file
+    link_to=~/.$file
+    if [[ "$file" == "flake8" ]]; then
+      link_to=~/.config/flake8
+    elif [[ "$file" == "sshconfig" ]]; then
+      link_to=~/.ssh/config
     fi
+
+    if [ ! -L $link_to ]; then
+      echo "Linking $link_from to $link_to..."
+      ln -s "$link_from" "$link_to"
+    fi
+  fi
 done
