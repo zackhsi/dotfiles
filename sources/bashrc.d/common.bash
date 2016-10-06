6bca3c7c4e7f71789fefe2f2f04c831c9cd226dd
Zack Hsi <zackhsi@gmail.com>
2016-10-06 10:22:12 -0700

    Sort

--- sources/bashrc.d/common.bash
+++ sources/bashrc.d/common.bash
@@ -1,25 +1,19 @@
 # Common zsh/bash configuration
 
-export EDITOR='nvim'
-export PAGER="less -IM"
-
-if [ -n "$TMUX" ]; then
-  export TERM=screen-256color
-else
-  export TERM=xterm-256color
-fi
-
 # Code
 export SRC=$HOME/src
 
-# Search
-export ACK_OPTIONS='--ignore-dir=venv --ignore-file=is:tags'
-export GREP_OPTIONS='--exclude-dir=venv --exclude=tags --color=auto'
+# Devbox
+alias c=control
+export DOCKER_HOST="tcp://127.0.0.1:2375"
+
+# Editor
+export EDITOR='nvim'
 
 # FZF
 export FZF_DEFAULT_COMMAND='ag -l -U -g ""'
 
-# Git aliases
+# Git
 alias g='git'
 alias gc!='git commit -v --amend'
 alias gc='git commit -v'
@@ -38,36 +32,32 @@ alias gp='git push'
 alias gst='git status'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip--"'
 
-# Tmux
-alias ta='tmux attach -t'
-alias tad='tmux attach -d -t'
-alias ts='tmux new-session -s'
-alias tl='tmux list-sessions'
-alias tksv='tmux kill-server'
-alias tkss='tmux kill-session -t'
-
-# GNU Coreutils
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
 # Golang
 export GOPATH=$HOME/golang
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 
-# Python
-export PYTHONDONTWRITEBYTECODE=1
+# GNU Coreutils
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 # Neovim
 alias v=nvim
 alias vi=nvim
 alias vim=nvim
 
-# thefuck
-alias fk=fuck
+# Pager
+export PAGER="less -IM"
 
-# shellcheck
+# Python
+export PYTHONDONTWRITEBYTECODE=1
+
+# Search
+export ACK_OPTIONS='--ignore-dir=venv --ignore-file=is:tags'
+export GREP_OPTIONS='--exclude-dir=venv --exclude=tags --color=auto'
+
+# Shellcheck
 export SHELLCHECK_OPTS="-e SC1091"
 
 # ssh
@@ -75,6 +65,20 @@ if ! ssh-add -l > /dev/null; then
   ssh-add 2>/dev/null
 fi
 
-# Devbox
-alias c=control
-export DOCKER_HOST="tcp://127.0.0.1:2375"
+# TERM
+if [ -n "$TMUX" ]; then
+  export TERM=screen-256color
+else
+  export TERM=xterm-256color
+fi
+
+# thefuck
+alias fk=fuck
+
+# Tmux
+alias ta='tmux attach -t'
+alias tad='tmux attach -d -t'
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
