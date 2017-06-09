2a5f190ed4aefb644b20b9d6b79c714333df306a
Zack Hsi <zackhsi@gmail.com>
2017-06-09 10:25:18 -0700

    Do not confirm exit

--- sources/ipython_config.py
+++ sources/ipython_config.py
@@ -282,7 +282,7 @@ c.InteractiveShell.colors = 'Linux'
 ## Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
 #  Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
 #  direct exit without any confirmation.
-#c.TerminalInteractiveShell.confirm_exit = True
+c.TerminalInteractiveShell.confirm_exit = False
 
 ## Options for displaying tab completions, 'column', 'multicolumn', and
 #  'readlinelike'. These options are for `prompt_toolkit`, see `prompt_toolkit`
