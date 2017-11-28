f887e3757341fc80e88b3770e0d472519d15070f
Zack Hsi <zackhsi@gmail.com>
2017-11-28 10:10:52 -0800

    Update control completion
    Use ssh to access docker, update commands, fix lint.

--- sources/bash_completion.d/control.bash
+++ sources/bash_completion.d/control.bash
@@ -1,17 +1,19 @@
 have docker && have control &&
 _control() {
     COMPREPLY=()
-    local cur prev opts cword
+    local cur prev cword
     _get_comp_words_by_ref cur prev words cword
     if [[ $cword -eq 1 ]]; then
       # control -h | sed -n '/for detailed help/,$p' | tail -n+2 | awk '{print $1}'
-      read -d '' commands << EOF
+      read -rd '' commands << EOF
 ancestors
 build
 check
 clean
 deploy
 descendants
+disconnect
+disconnects
 discover
 ensure
 enter
@@ -25,6 +27,8 @@ present
 provision
 pull
 push
+register-host
+replicate-images
 release
 reset
 restart
@@ -35,16 +39,17 @@ stats
 status
 stop
 test
+tethered
 update
 url
 version
 EOF
-      COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+      COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
       return 0
     elif [[ $cword -eq 2 ]]; then
       if [[ "$prev" =~ enter|kill|restart|stop ]]; then
-        running_containers=$(docker ps --format "{{.Names}}")
-        COMPREPLY=( $(compgen -W "${running_containers}" -- ${cur}) )
+        running_containers=$("$SRC/devbox/bin/devbox-ssh-fast" sudo docker ps --format "{{.Names}}" | tr -d '\r')
+        COMPREPLY=( $(compgen -W "$running_containers" -- "$cur") )
         return 0
       fi
     fi
