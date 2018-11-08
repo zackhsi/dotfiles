0b987a02bb99984051abae6f3ed2306bfc42c6ca
Zack Hsi <zackhsi@gmail.com>
2018-11-07 18:51:58 -0800

    Unfuck stripe profile

--- sources/bashrc.d/stripe.bash
+++ sources/bashrc.d/stripe.bash
@@ -1,4 +1,5 @@
-source ~/.stripe_profile
+[ -f ~/stripe/password-vault/bash_completion ] && . ~/stripe/password-vault/bash_completion
+[ -x ~/stripe/space-commander/bin/sc-ssh-wrapper ] && alias ssh="TERM=xterm-color sc-ssh-wrapper"
 export PATH="~/stripe/henson/bin:$PATH"
 export PATH="~/stripe/password-vault/bin:$PATH"
 export PATH="~/stripe/space-commander/bin:$PATH"
