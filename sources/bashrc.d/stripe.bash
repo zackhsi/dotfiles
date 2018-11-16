d4c6b693a8de101475e84e0634a19282f7848b55
Zack Hsi <zackhsi@gmail.com>
2018-11-16 12:49:57 -0800

    Hard-code SC_AWS_ROLE_NAME

--- sources/bashrc.d/stripe.bash
+++ sources/bashrc.d/stripe.bash
@@ -1,5 +1,8 @@
 [ -f ~/stripe/password-vault/bash_completion ] && . ~/stripe/password-vault/bash_completion
 [ -x ~/stripe/space-commander/bin/sc-ssh-wrapper ] && alias ssh="TERM=xterm-color sc-ssh-wrapper"
+
 export PATH="~/stripe/henson/bin:$PATH"
 export PATH="~/stripe/password-vault/bin:$PATH"
 export PATH="~/stripe/space-commander/bin:$PATH"
+
+export SC_AWS_ROLE_NAME=access-s3-stripe-data
