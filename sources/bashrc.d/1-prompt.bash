d0cfd7f404f1d4e1a8d543915122532f96954428
Zack Hsi <zackhsi@gmail.com>
2018-11-23 09:23:35 -0800

    Simplify and speed up prompt

--- sources/bashrc.d/1-prompt.bash
+++ sources/bashrc.d/1-prompt.bash
@@ -1,54 +1,53 @@
-__set_bash_prompt()
-{
-  GIT_PS1_SHOWCOLORHINTS=true
-  if [[ $(pwd) = "/Users/zackhsi/stripe/pay-server" ]]; then
-    GIT_PS1_SHOWDIRTYSTATE=
-    GIT_PS1_SHOWUNTRACKEDFILES=
-  else
-    GIT_PS1_SHOWDIRTYSTATE=true
-    GIT_PS1_SHOWUNTRACKEDFILES=true
+__git_branch_fast() {
+  local gitdir=""
+  local cur="$PWD"
+  while [[ ! -z "$cur" ]]; do
+    if [[ -e "$cur/.git" ]]; then
+      gitdir="$cur/.git"
+      break
+    fi
+    cur="${cur%/*}"
+  done
+  [ -f "$gitdir/HEAD" ] && read -r head < "$gitdir/HEAD"
+  case "$head" in
+    ref:*)
+      branch="${head##*/}"
+      ;;
+    "")
+      branch=""
+      ;;
+    *)
+      branch="d:${head:0:7}"
+      ;;
+  esac
+  if [ -n "$branch" ]; then
+    echo " ($branch)"
   fi
+}
 
+
+__set_bash_prompt() {
   local exit="$?" # Save the exit status of the last command
 
-  # PS1 is made from $PreGitPS1 + <git-status> + $PostGitPS1
-  local PreGitPS1=""
-  local PostGitPS1=""
-
-  # Wrap the colour codes between \[ and \], so that
-  # bash counts the correct number of characters for line wrapping:
-  local Red='\[\e[0;31m\]'
-  local Gre='\[\e[0;32m\]'
-  local Yel='\[\e[0;33m\]'
-  local Blu='\[\e[0;34m\]'
-  local Mag='\[\e[0;35m\]'
-  local Cya='\[\e[0;36m\]'
-  local Whi='\[\e[0;37m\]'
-  local Bold='\[\e[1m\]'
-  local None='\[\e[0m\]' # Return to default colour
-
-  PreGitPS1+="$Mag$Bold\W$None"
-
-  # Virtualenv
-  if [ -n "$VIRTUAL_ENV" ]; then
-    PostGitPS1+=" ($(basename $VIRTUAL_ENV))"
-  fi
+  local RED='\[\e[0;31m\]'
+  local GRE='\[\e[0;32m\]'
+  local YEL='\[\e[0;33m\]'
+  local BLU='\[\e[0;34m\]'
+  local MAG='\[\e[0;35m\]'
+  local CYA='\[\e[0;36m\]'
+  local WHI='\[\e[0;37m\]'
+  local BOLD='\[\e[1m\]'
+  local NONE='\[\e[0m\]'
 
   # Highlight non-standard exit codes
   if [[ $exit != 0 ]]; then
-    PostGitPS1+=" $Red$Bold[$exit]"
-  fi
-
-  if [[ $exit == 0 ]]; then
-    PostGitPS1+="$Blu$Bold"' $ '"$None"
+    EXIT_STATUS=" ${RED}${BOLD}[$exit]"
   else
-    PostGitPS1+="$Red$Bold"' $ '"$None"
+    EXIT_STATUS="$MAG$BOLD"
   fi
+  PS1="${CYA}${BOLD}"'\W'"${YEL}$(__git_branch_fast)""${EXIT_STATUS}"' $ '"$NONE"
 
-  # Set PS1 from $PreGitPS1 + <git-status> + $PostGitPS1
-  __git_ps1 "$PreGitPS1" "$PostGitPS1" ' (%s)'
+  export PS1
 }
 
-# This tells bash to reinterpret PS1 after every command, which we
-# need because __git_ps1 will return different text and colors
-PROMPT_COMMAND="__set_bash_prompt; $PROMPT_COMMAND"
+PROMPT_COMMAND="__set_bash_prompt; history -n; history -w; history -c; history -r;"
