da7d44cfa7bce06bbc5d5558698b859e2c458f5a
Zack Hsi <zackhsi@gmail.com>
2019-04-14 10:21:22 -0700

    ssh opsbox

--- sources/bashrc.d/stripe.bash
+++ sources/bashrc.d/stripe.bash
@@ -1,10 +1,15 @@
 [ -f ~/stripe/password-vault/bash_completion ] && . ~/stripe/password-vault/bash_completion
 [ -x ~/stripe/space-commander/bin/sc-ssh-wrapper ] && alias ssh="TERM=xterm-color sc-ssh-wrapper"
 
-export PATH="~/stripe/henson/bin:$PATH"
-export PATH="~/stripe/password-vault/bin:$PATH"
-export PATH="~/stripe/space-commander/bin:$PATH"
+export PATH="$HOME/stripe/henson/bin:$PATH"
+export PATH="$HOME/stripe/password-vault/bin:$PATH"
+export PATH="$HOME/stripe/space-commander/bin:$PATH"
 
 export SC_AWS_ROLE_NAME=engineersreadonly
 
 alias pay="TERM=xterm-color pay"
+
+opsbox() {
+  ls-servers -Spt opsbox --sample-user -sN
+  ssh "$(ls-servers -Spt opsbox --sample-user -sN)"
+}
