273f5c2ab049123205a7f70835085dc2f5f9109b
Zack Hsi <zackhsi@gmail.com>
2019-06-21 11:34:54 +0100

    Remove hardcoded SC role
    Use `sc-aws-console` instead.

--- sources/bashrc.d/1-stripe.bash
+++ sources/bashrc.d/1-stripe.bash
@@ -5,8 +5,6 @@ export PATH=$HOME/stripe/space-commander/bin:$PATH
 [ -f ~/stripe/password-vault/bash_completion ] && . ~/stripe/password-vault/bash_completion
 [ -x ~/stripe/space-commander/bin/sc-ssh-wrapper ] && alias ssh="TERM=xterm-color sc-ssh-wrapper"
 
-export SC_AWS_ROLE_NAME=engineersreadonly
-
 alias pay="TERM=xterm-color pay"
 
 ssh-service() {
