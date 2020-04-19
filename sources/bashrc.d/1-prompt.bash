aa505fc560ed607d8e3aca0ac69203a69827b21a
Zack Hsi <zackhsi@gmail.com>
2020-04-19 23:14:32 +0800

    Prompt update! With time :)

--- sources/bashrc.d/1-prompt.bash
+++ sources/bashrc.d/1-prompt.bash
@@ -1,9 +1,74 @@
-__git_branch_fast() {
-  local YEL='\[\e[0;33m\]'
-  local GRE='\[\e[0;32m\]'
-  local ITALIC='\[\e[3m\]'
-  local NONE='\[\e[0m\]'
+RED='\[\e[0;31m\]'
+GRE='\[\e[0;32m\]'
+YEL='\[\e[0;33m\]'
+BLU='\[\e[0;34m\]'
+MAG='\[\e[0;35m\]'
+CYA='\[\e[0;36m\]'
+NO_COLOUR="\e[00m"
 
+ITALIC='\[\e[3m\]'
+BOLD='\[\e[1m\]'
+
+SINGLE_SPACE=" "
+DOUBLE_SPACE="  "
+NEWLINE="\n"
+PRINTING_OFF="\["
+PRINTING_ON="\]"
+PROMPT_COLOUR="${MAG}${BOLD}"
+TIMESTAMP_COLOUR="${BOLD}"
+PS1_PROMPT="\$"
+PS2_PROMPT=">"
+RESTORE_CURSOR_POSITION="\e[u"
+SAVE_CURSOR_POSITION="\e[s"
+TIMESTAMP="\A"
+TIMESTAMP_PLACEHOLDER="--:--"
+
+# https://redandblack.io/blog/2020/bash-prompt-with-updating-time/
+move_cursor_to_start_of_ps1() {
+  command_rows=$(history 1 | wc -l)
+  if [ "$command_rows" -gt 1 ]; then
+    let vertical_movement=$command_rows+1
+  else
+    command=$(history 1 | sed 's/^\s*[0-9]*\s*//')
+    command_length=${#command}
+    ps1_prompt_length=${#PS1_PROMPT}
+    let total_length=$command_length+$ps1_prompt_length
+    let lines=$total_length/${COLUMNS}+1
+    let vertical_movement=$lines+1
+  fi
+  tput cuu $vertical_movement
+}
+
+__prompt() {
+  PS1_ELEMENTS=(
+      # Empty line after last command.
+      "$EXIT_STATUS"
+      "$NEWLINE"
+      # First line of prompt.
+      "$PRINTING_OFF" "$BOLD" "$PRINTING_ON"
+      "$TIMESTAMP_PLACEHOLDER" "$PRINTING_OFF"
+      "$DOUBLE_SPACE"  "$GIT_BRANCH" "$PRINTING_OFF"
+      "$NO_COLOUR" "$PRINTING_ON" "$NEWLINE"
+      # Second line of prompt.
+      "$PRINTING_OFF" "$PROMPT_COLOUR" "$PRINTING_ON" "$PS1_PROMPT"
+      "$SINGLE_SPACE" "$PRINTING_OFF" "$NO_COLOUR" "$PRINTING_ON"
+  )
+  PS1=$(IFS=; echo "${PS1_ELEMENTS[*]}")
+
+  PS2_ELEMENTS=(
+      "$PRINTING_OFF" "$PROMPT_COLOUR" "$PRINTING_ON" "$PS2_PROMPT"
+      "$SINGLE_SPACE" "$PRINTING_OFF" "$NO_COLOUR" "$PRINTING_ON"
+  )
+  PS2=$(IFS=; echo "${PS2_ELEMENTS[*]}")
+
+  PS0_ELEMENTS=(
+    "$SAVE_CURSOR_POSITION" "\$(move_cursor_to_start_of_ps1)"
+    "$TIMESTAMP_COLOUR" "$TIMESTAMP" "$NO_COLOUR" "$RESTORE_CURSOR_POSITION"
+  )
+  PS0=$(IFS=; echo "${PS0_ELEMENTS[*]}")
+}
+
+__git_branch_fast() {
   local gitdir=""
   local cur="$PWD"
   while [[ ! -z "$cur" ]]; do
@@ -26,32 +91,25 @@ __git_branch_fast() {
       ;;
   esac
   if [ -n "$branch" ]; then
-    echo " ${YEL}${ITALIC}(${GRE}${ITALIC}$branch${YEL}${ITALIC})${NONE}"
+    echo "${DOUBLE_SPACE}${YEL}${ITALIC}(${GRE}${ITALIC}$branch${YEL}${ITALIC})${NO_COLOUR}"
   fi
 }
 
+__export_git_branch() {
+  GIT_BRANCH="${CYA}${BOLD}"'\W'"${YEL}$(__git_branch_fast)${NO_COLOUR}"
+  export GIT_BRANCH
+}
 
-__set_bash_prompt() {
+__export_exit_status() {
   local exit="$?" # Save the exit status of the last command
 
-  local RED='\[\e[0;31m\]'
-  local GRE='\[\e[0;32m\]'
-  local YEL='\[\e[0;33m\]'
-  local MAG='\[\e[0;35m\]'
-  local CYA='\[\e[0;36m\]'
-  local BOLD='\[\e[1m\]'
-  local NONE='\[\e[0m\]'
-
-  # Highlight non-standard exit codes
+  # Highlight non-zero exit codes.
   if [[ $exit != 0 ]]; then
-    EXIT_STATUS=" ${RED}${BOLD}[$exit]"
+    EXIT_STATUS="${RED}${BOLD}→ Exit status: $exit${NO_COLOUR}${NEWLINE}"
   else
-    EXIT_STATUS="$MAG$BOLD"
+    EXIT_STATUS=""
   fi
-  ARROWS="${RED}"$'\u227b'"${YEL}"$'\u227b'"${GRE}"$'\u227b'
-  PS1="${CYA}${BOLD}"'\W'"${YEL}$(__git_branch_fast)${EXIT_STATUS} ${ARROWS} ${NONE}"
-
-  export PS1
+  export EXIT_STATUS
 }
 
-PROMPT_COMMAND="__set_bash_prompt; history -n; history -w; history -c; history -r;"
+PROMPT_COMMAND="__export_exit_status; __export_git_branch; __prompt;"
