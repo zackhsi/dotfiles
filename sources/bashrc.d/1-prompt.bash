3cdb3cf78bf9c921a065da521911c947b16ddcf0
Zack Hsi <zackhsi@gmail.com>
2020-04-20 11:00:47 +0800

    Fix printing artifacts, refactor
    https://superuser.com/questions/382456/why-does-this-bash-prompt-sometimes-keep-part-of-previous-commands-when-scrollin

--- sources/bashrc.d/1-prompt.bash
+++ sources/bashrc.d/1-prompt.bash
@@ -1,10 +1,19 @@
+# Configure PS0, PS1, and PS2.
+#
+# Dynamically generates via PROMPT_COMMAND:
+# - Timestamp that previous command ran [0].
+# - Exit status of previous command if nonzero.
+# - Git branch (fast).
+#
+# [0] From: https://redandblack.io/blog/2020/bash-prompt-with-updating-time/
+
 RED='\[\e[0;31m\]'
 GRE='\[\e[0;32m\]'
 YEL='\[\e[0;33m\]'
 BLU='\[\e[0;34m\]'
 MAG='\[\e[0;35m\]'
 CYA='\[\e[0;36m\]'
-NO_COLOUR="\e[00m"
+NO_COLOR='\[\e[0m\]'
 
 ITALIC='\[\e[3m\]'
 BOLD='\[\e[1m\]'
@@ -12,66 +21,67 @@ BOLD='\[\e[1m\]'
 SINGLE_SPACE=" "
 DOUBLE_SPACE="  "
 NEWLINE="\n"
-PRINTING_OFF="\["
-PRINTING_ON="\]"
-PROMPT_COLOUR="${MAG}${BOLD}"
-TIMESTAMP_COLOUR="${BOLD}"
+PROMPT_COLOR="${MAG}${BOLD}"
+TIMESTAMP_COLOR="${BOLD}"
 PS1_PROMPT="\$"
 PS2_PROMPT=">"
 RESTORE_CURSOR_POSITION="\e[u"
 SAVE_CURSOR_POSITION="\e[s"
 TIMESTAMP="\A"
+DIRECTORY='\w'
 TIMESTAMP_PLACEHOLDER="--:--"
 
-# https://redandblack.io/blog/2020/bash-prompt-with-updating-time/
 move_cursor_to_start_of_ps1() {
   command_rows=$(history 1 | wc -l)
   if [ "$command_rows" -gt 1 ]; then
-    let vertical_movement=$command_rows+1
+    vertical_movement=$(( command_rows + 1 ))
   else
     command=$(history 1 | sed 's/^\s*[0-9]*\s*//')
     command_length=${#command}
     ps1_prompt_length=${#PS1_PROMPT}
-    let total_length=$command_length+$ps1_prompt_length
-    let lines=$total_length/${COLUMNS}+1
-    let vertical_movement=$lines+1
+    total_length=$(( command_length + ps1_prompt_length ))
+    lines=$(( total_length / COLUMNS + 1))
+    vertical_movement=$(( lines + 1 ))
   fi
   tput cuu $vertical_movement
 }
 
 __prompt() {
   PS1_ELEMENTS=(
-      # Empty line after last command.
-      "$EXIT_STATUS"
-      "$NEWLINE"
-      # First line of prompt.
-      "$PRINTING_OFF" "$BOLD" "$PRINTING_ON"
-      "$TIMESTAMP_PLACEHOLDER" "$PRINTING_OFF"
-      "$DOUBLE_SPACE"  "$GIT_BRANCH" "$PRINTING_OFF"
-      "$NO_COLOUR" "$PRINTING_ON" "$NEWLINE"
-      # Second line of prompt.
-      "$PRINTING_OFF" "$PROMPT_COLOUR" "$PRINTING_ON" "$PS1_PROMPT"
-      "$SINGLE_SPACE" "$PRINTING_OFF" "$NO_COLOUR" "$PRINTING_ON"
+    # Previous command's exit status.
+    "$EXIT_STATUS"
+    # Empty line after last command.
+    "$NEWLINE"
+    # First line of prompt.
+    "$BOLD"
+    "$TIMESTAMP_PLACEHOLDER" "$DOUBLE_SPACE"
+    "$CYA" "$BOLD" "$DIRECTORY" "$DOUBLE_SPACE"
+    "$GIT_BRANCH"
+    "$NO_COLOR" "$NEWLINE"
+    # Second line of prompt.
+    "$PROMPT_COLOR" "$PS1_PROMPT"
+    "$SINGLE_SPACE" "$NO_COLOR"
   )
   PS1=$(IFS=; echo "${PS1_ELEMENTS[*]}")
 
   PS2_ELEMENTS=(
-      "$PRINTING_OFF" "$PROMPT_COLOUR" "$PRINTING_ON" "$PS2_PROMPT"
-      "$SINGLE_SPACE" "$PRINTING_OFF" "$NO_COLOUR" "$PRINTING_ON"
+    "$PROMPT_COLOR" "$PS2_PROMPT"
+    "$SINGLE_SPACE" "$NO_COLOR"
   )
   PS2=$(IFS=; echo "${PS2_ELEMENTS[*]}")
 
   PS0_ELEMENTS=(
     "$SAVE_CURSOR_POSITION" "\$(move_cursor_to_start_of_ps1)"
-    "$TIMESTAMP_COLOUR" "$TIMESTAMP" "$NO_COLOUR" "$RESTORE_CURSOR_POSITION"
+    "$TIMESTAMP_COLOR" "$TIMESTAMP" "$NO_COLOR" "$RESTORE_CURSOR_POSITION"
   )
   PS0=$(IFS=; echo "${PS0_ELEMENTS[*]}")
+  export PS0
 }
 
-__git_branch_fast() {
+__compute_git_branch_fast() {
   local gitdir=""
   local cur="$PWD"
-  while [[ ! -z "$cur" ]]; do
+  while [[ -n "$cur" ]]; do
     if [[ -e "$cur/.git" ]]; then
       gitdir="$cur/.git"
       break
@@ -91,25 +101,22 @@ __git_branch_fast() {
       ;;
   esac
   if [ -n "$branch" ]; then
-    echo "${DOUBLE_SPACE}${YEL}${ITALIC}(${GRE}${ITALIC}$branch${YEL}${ITALIC})${NO_COLOUR}"
+    GIT_BRANCH="${YEL}${ITALIC}(${GRE}${ITALIC}$branch${YEL}${ITALIC})${NO_COLOR}"
+  else
+    GIT_BRANCH=""
   fi
 }
 
-__export_git_branch() {
-  GIT_BRANCH="${CYA}${BOLD}"'\w'"${YEL}$(__git_branch_fast)${NO_COLOUR}"
-  export GIT_BRANCH
-}
-
-__export_exit_status() {
-  local exit="$?" # Save the exit status of the last command
+__compute_exit_status() {
+  local exit="$?"
 
   # Highlight non-zero exit codes.
   if [[ $exit != 0 ]]; then
-    EXIT_STATUS="${RED}${BOLD}→ Exit status: $exit${NO_COLOUR}${NEWLINE}"
+    EXIT_STATUS="${RED}${BOLD}→ Exit status: $exit${NO_COLOR}${NEWLINE}"
   else
     EXIT_STATUS=""
   fi
-  export EXIT_STATUS
 }
 
-PROMPT_COMMAND="__export_exit_status; __export_git_branch; __prompt; history -n; history -w; history -c; history -r;"
+# __compute_exit_status must be called first to save the previous exit status.
+PROMPT_COMMAND="__compute_exit_status; __compute_git_branch_fast; __prompt; history -n; history -w; history -c; history -r;"
