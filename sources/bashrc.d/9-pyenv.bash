382309253114b28a78c6689a50da1d3d4dabfe37
Zack Hsi <zackhsi@gmail.com>
2021-11-11 15:29:34 +0800

    Fix pyenv

--- sources/bashrc.d/9-pyenv.bash
+++ sources/bashrc.d/9-pyenv.bash
@@ -1,4 +1,6 @@
-eval "$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 
 # https://github.com/pyenv/pyenv/wiki#how-to-build-cpython-with-framework-support-on-os-x
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
