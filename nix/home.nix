28d5ab8ecfacb3212ae14c0c61003feadf99b1cc
Zack Hsi <zackhsi@gmail.com>
2025-06-19 20:58:43 -0700

    Start new configs for git and tmux

--- nix/home.nix
+++ nix/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.username = "zackhsi";
   home.homeDirectory = /Users/zackhsi;
@@ -6,5 +6,11 @@
   programs.home-manager.enable = true;
   home.packages = [
     pkgs.nixfmt-rfc-style
+    pkgs.tmux
   ];
+  home.file = {
+    ".tmux.conf" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/tmux.conf"; };
+    ".gitignore" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitignore"; };
+    ".gitconfig" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitconfig"; };
+  };
 }
