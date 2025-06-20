d07430c9933688776f4c8049bde2a77a0f9370b6
Zack Hsi <zackhsi@gmail.com>
2025-06-19 21:30:14 -0700

    Manage Cursor settings

--- nix/home.nix
+++ nix/home.nix
@@ -8,11 +8,13 @@
     pkgs.nixfmt-rfc-style
     pkgs.starship
     pkgs.tmux
+    pkgs.tree
   ];
   home.file = {
     ".gitconfig" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitconfig"; };
     ".gitignore" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitignore"; };
     ".tmux.conf" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/tmux.conf"; };
+    "Library/Application Support/Cursor/User/settings.json" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/cursor_settings.json"; };
     ".zshrc" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/zshrc"; };
   };
   programs.home-manager.enable = true;
