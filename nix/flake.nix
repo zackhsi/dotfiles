188de859fe08e71d9d0bf8457500db796958cab9
Zack Hsi <zackhsi@gmail.com>
2025-06-19 21:00:55 -0700

    Clean up

--- nix/flake.nix
+++ nix/flake.nix
@@ -12,60 +12,9 @@
   outputs = {nixpkgs, home-manager, ...}: {
     homeConfigurations = {
       zackhsi = home-manager.lib.homeManagerConfiguration {
-        # System is very important!
         pkgs = import nixpkgs { system = "aarch64-darwin"; };
-
-        modules = [ ./home.nix ]; # Defined later
+        modules = [ ./home.nix ];
       };
     };
   };
 }
-
-
-
-
-# {
-#   description = "My macOS Nix Configuration";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-#     home-manager = {
-#       url = "github:nix-community/home-manager";
-#       inputs.nixpkgs.follows = "nixpkgs";
-#     };
-#     darwin = {
-#       url = "github:lnl7/nix-darwin";
-#       inputs.nixpkgs.follows = "nixpkgs";
-#     };
-#   };
-
-#   outputs = { self, nixpkgs, home-manager, darwin }:
-#     let
-#       system = "aarch64-darwin";
-#       username = "zackhsi";
-#       configuration = { pkgs, config, ... }: {
-#         users = {
-#             users.${username} = {
-#               home = "/Users/${username}";
-#               name = "${username}";
-#             };
-#           };
-#       };  
-#     in {
-#       darwinConfigurations.${username} = darwin.lib.darwinSystem {
-#         inherit system;
-#         modules = [
-#           configuration
-#           ./darwin.nix
-#           home-manager.darwinModules.home-manager
-#           {
-#             home-manager = {
-#               useGlobalPkgs = true;
-#               useUserPackages = true;
-#               users.${username} = import ./home.nix;
-#             };
-#           }
-#         ];
-#       };
-#     };
-# } 
--- nix/home.nix
+++ nix/home.nix
@@ -3,7 +3,6 @@
   home.username = "zackhsi";
   home.homeDirectory = /Users/zackhsi;
   home.stateVersion = "25.05";
-  programs.home-manager.enable = true;
   home.packages = [
     pkgs.nixfmt-rfc-style
     pkgs.tmux
@@ -13,4 +12,5 @@
     ".gitignore" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitignore"; };
     ".gitconfig" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitconfig"; };
   };
+  programs.home-manager.enable = true;
 }
