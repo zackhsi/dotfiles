f156d4e65801a3dfea5b74c8d570988776ab4960
Zack Hsi <zackhsi@gmail.com>
2025-06-19 21:18:57 -0700

    Install Neovim and Starship

--- nix/home.nix
+++ nix/home.nix
@@ -4,13 +4,16 @@
   home.homeDirectory = /Users/zackhsi;
   home.stateVersion = "25.05";
   home.packages = [
+    pkgs.neovim
     pkgs.nixfmt-rfc-style
+    pkgs.starship
     pkgs.tmux
   ];
   home.file = {
-    ".tmux.conf" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/tmux.conf"; };
-    ".gitignore" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitignore"; };
     ".gitconfig" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitconfig"; };
+    ".gitignore" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/gitignore"; };
+    ".tmux.conf" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/tmux.conf"; };
+    ".zshrc" = { source = config.lib.file.mkOutOfStoreSymlink "/Users/zackhsi/dotfiles/nix/sources/zshrc"; };
   };
   programs.home-manager.enable = true;
 }
