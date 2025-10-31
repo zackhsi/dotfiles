202f4b09e5fa2f36f45da4fc719852e38882c0b0
Zack Hsi <zackhsi@gmail.com>
2025-10-31 16:31:32 +0800

    Ghostty + zsh

---
+++ nix/home.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ $(hostname) != st* ]]; then
+  echo "home.sh is only for Stripe laptop"
+  exit 1
+fi
+
+ln -sf ~/dotfiles/nix/sources/ghostty_config ~/Library/Application\ Support/com.mitchellh.ghostty/config
+ln -sf ~/dotfiles/nix/sources/zshrc ~/.zshrc
+
+command -v starship > /dev/null || brew install starship
+[[ -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] || brew install zsh-autosuggestions
