f63f2b69dbe148c804a66e70e533b571f45809ef
Zack Hsi <zackhsi@gmail.com>
2025-11-04 09:34:30 +0800

    Use new, simplified tmux

--- nix/home.sh
+++ nix/home.sh
@@ -8,6 +8,7 @@ fi
 
 ln -sf ~/dotfiles/nix/sources/ghostty_config ~/Library/Application\ Support/com.mitchellh.ghostty/config
 ln -sf ~/dotfiles/nix/sources/zshrc ~/.zshrc
+ln -sf ~/dotfiles/nix/sources/tmux.conf ~/.tmux.conf
 
 command -v starship > /dev/null || brew install starship
 [[ -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] || brew install zsh-autosuggestions
