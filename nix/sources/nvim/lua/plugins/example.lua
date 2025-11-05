586dea85b645d2c072629316d3c87628eba96b07
Zack Hsi <zackhsi@gmail.com>
2025-11-05 15:11:15 +0800

    Configure telescope
    It uses C-f to fuzzy find, but it's buggy when the original search is
    slow and ongoing.

--- nix/sources/nvim/lua/plugins/example.lua
+++ nix/sources/nvim/lua/plugins/example.lua
@@ -37,29 +37,6 @@ return {
     end,
   },
 
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-
   -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
