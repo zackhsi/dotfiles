5834b9a1f46819139aa92f095b5971dfe89c4d8c
Zack Hsi <zackhsi@gmail.com>
2025-11-11 12:07:13 +0800

    Faster animated scrolling

---
+++ nix/sources/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = true,
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/animate.md
+      animate = { duration = 5 },
+    },
+  },
+}
